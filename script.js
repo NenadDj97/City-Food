7f17842ffbd289fda73662cb56a003d60c2f61d1
NenadDj97 <93268425+NenadDj97@users.noreply.github.com>
2021-12-24 18:06:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ $("#back").click(function() {
         scrollTop: $("#logo").offset().top
     }, 2000);
 });
+
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
